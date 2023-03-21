4253572f7079e7ec8efa35dee2332d4defa58bf1
Pylex99 <117868566+Pylex99@users.noreply.github.com>
2023-03-21 13:15:22 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,23 +1,25 @@
-import requests
-from bs4 import BeautifulSoup
-from gtts import gTTS
+import requests  # Used to make HTTP requests
+from bs4 import BeautifulSoup  # Used to parse HTML content
+from gtts import gTTS  # Used to convert text to audio
 
 
+# Takes input URL from the user, accesses the URL, parses the HTML content and converts text to audio
 def main():
     url = input("Enter URL of the webpage: ")
     corrected_url = user_url(url)
     html_content = access_url(corrected_url)
     text = parse_html(html_content)
     text_to_audio(text)
-    #play_audio(text_to_audio)
-
-
+    
+    
+# Corrects the URL entered by the user if it doesn't start with http or https
 def user_url(url):
     if not url.startswith('http|https'):
         #print('http://' + url)
         return 'http://' + url
 
 
+# Accesses the URL and gets the HTML content
 def access_url(url):
     try:
         response = requests.get(url)
@@ -30,6 +32,7 @@ def access_url(url):
         print("Invalid URL")
 
 
+# Parses the HTML content and gets the text
 def parse_html(html_content):
     soup = BeautifulSoup(html_content.content, 'html.parser')
     text = soup.get_text()
@@ -37,6 +40,7 @@ def parse_html(html_content):
     return text
 
 
+# Converts text to audio and saves the audio as a file
 def text_to_audio(text):
     tts = gTTS(text, lang="en")
     tts.save("text_to_audio.mp3")
